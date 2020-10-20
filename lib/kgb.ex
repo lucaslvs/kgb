b0858d9064988115a05332e909175d0ff02ec9ce
Lucas Varlesse <lucas.varlesse@redealumni.com>
2020-10-20 20:40:01 -0300

    initial commit

---
+++ lib/kgb.ex
@@ -0,0 +1,18 @@
+defmodule KGB do
+  @moduledoc """
+  Documentation for `KGB`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KGB.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/kgb/application.ex
@@ -0,0 +1,19 @@
+defmodule KGB.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: KGB.Worker.start_link(arg)
+      # {KGB.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: KGB.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
