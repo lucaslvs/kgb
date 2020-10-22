e255c722cc4b6429671b328c99b420b1b09adf9c
Lucas Varlesse <lucas.varlesse@redealumni.com>
2020-10-21 22:23:52 -0300

    create KGB.Employee.create/1 function and define KGB.Employee.t() type

--- lib/kgb/employee.ex
+++ lib/kgb/employee.ex
@@ -2,4 +2,31 @@ defmodule KGB.Employee do
   @enforce_keys [:name, :rating]
 
   defstruct @enforce_keys
+
+  @type t() :: %KGB.Employee{name: binary(), rating: integer()}
+
+  @doc """
+  Create a `KGB.Employee` by the given `parameters`.
+
+  ## Examples
+
+      iex> KGB.Employee.create(%{name: "Lucas", rating: 50})
+      {:ok %KGB.Employee{name: "Lucas", rating: 50}}
+
+      iex> KGB.Employee.create(%{rating: 50})
+      {:error, {:validation, %{name: ["is required"]}}}
+
+      iex> KGB.Employee.create(%{name: "Lucas"})
+      {:error, {:validation, %{name: ["is required"]}}}
+
+      iex> KGB.Employee.create(%{name: "Lucas", rating: 51})
+      {:error, {:validation, %{rating: ["must be less than or equal to 50; got: 51"]}}}
+
+      iex> KGB.Employee.create(%{name: "Lucas", rating: -1})
+      {:error, {:validation, %{rating: ["must be greater than or equal to 0; got: -1"]}}}
+  """
+  @spec create(map()) :: {:ok, KGB.Employee.t()} | {:error, any()}
+  def create(parameters) when is_map(parameters) do
+    __MODULE__.Create.run(parameters)
+  end
 end
