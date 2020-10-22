97ea8771b99afdfc28c893d9dc222154af856383
Lucas Varlesse <lucas.varlesse@redealumni.com>
2020-10-21 22:44:27 -0300

    add @moduledoc and @type t() in KGB.Review module

--- lib/kgb/review.ex
+++ lib/kgb/review.ex
@@ -1,4 +1,8 @@
 defmodule KGB.Review do
+  @moduledoc """
+  This module is responsible for model a review post in `https://www.dealerrater.com/`.
+  """
+
   @enforce_keys [
     :from,
     :content,
@@ -14,4 +18,18 @@ defmodule KGB.Review do
   ]
 
   defstruct @enforce_keys
+
+  @type t() :: %KGB.Review{
+          from: binary(),
+          content: binary(),
+          rating: integer(),
+          published_at: binary(),
+          custom_service: integer(),
+          quality_of_work: integer(),
+          friendliness: integer(),
+          pricing: integer(),
+          overall_experience: integer(),
+          recommendend_dealer: binary(),
+          mentioned_employees: list(KGB.Employee.t())
+        }
 end
