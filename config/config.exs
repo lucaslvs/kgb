c64aeba77e25e92e27eb743b1b2d847be70dfde7
Lucas Varlesse <lucas.varlesse@redealumni.com>
2020-10-23 16:52:37 -0300

    refactoring configuration

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,26 @@
 use Mix.Config
 
+dealer_path =
+  System.get_env(
+    "DEALER_PATH",
+    "McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685"
+  )
+
+page_number = String.to_integer(System.get_env("PAGE_NUMBER", "5"))
+
+if page_number < 1 do
+  raise """
+  Environment variable PAGE_NUMBER should be equal or greater than 1.
+  """
+end
+
+config :kgb, KGB.Spider,
+  base_url: "https://www.dealerrater.com",
+  dealer_path: dealer_path,
+  page_number: page_number
+
 config :crawly,
-  concurrent_requests_per_domain: 5,
+  concurrent_requests_per_domain: page_number,
   middlewares: [
     Crawly.Middlewares.DomainFilter,
     Crawly.Middlewares.UniqueRequest,
@@ -25,12 +44,3 @@ config :crawly,
     Crawly.Pipelines.JSONEncoder,
     {Crawly.Pipelines.WriteToFile, extension: "json", folder: "./", include_timestamp: false}
   ]
-
-config :kgb, KGB.Spider,
-  base_url: "https://www.dealerrater.com",
-  dealer_path:
-    System.get_env(
-      "DEALER_PATH",
-      "McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685"
-    ),
-  page_number: String.to_integer(System.get_env("PAGE_NUMBER", "5"))
