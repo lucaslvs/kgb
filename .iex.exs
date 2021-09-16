178d9b4f7be19b09f38f7416d7aa3cfc2c9df7ea
Lucas Varlesse <lucas.vasilva@stone.com.br>
2021-09-16 18:35:32 -0300

    update .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,5 +1,1 @@
 alias KGB.{Employee, Review, Spider}
-
-{:ok, %HTTPoison.Response{body: body}} = HTTPoison.get("https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/page5")
-{:ok, document} = Floki.parse_document(body)
-review_document = document |> Floki.find("div.review-entry")
